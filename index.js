d4c77b51c241a6e53f14cb32a185762f132be646
sunsplat <heeycutie@yahoo.com>
2016-10-13 21:27:21 -0400

    solved api call

--- index.js
+++ index.js
@@ -1,31 +1,55 @@
-
-
 function searching() {
-	var searchQuery = document.getElementById('search-bar').value;
-	console.log('this is search'+searchQuery);
-	var searchLink = 'https://en.wikipedia.org/w/api.php?action=opensearch&search='+searchQuery+'&limit=1&namespace=0&format=jsonp';
+  var searchQuery = document.getElementById('search-bar').value;
+  var searchLink = 'https://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts%7Cimageinfo&generator=search&exsentences=2&exlimit=20&exintro=1&gsrsearch='+searchQuery+'&gsrlimit=20&origin=*&gsrprop=snippet%7Csectionsnippet%7Csectiontitle';
 
-	//AJAX call using pure JS
-	/**var xhttp = new XMLHttpRequest();
+  //AJAX call using pure JS
+  var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (xhttp.readyState == 4 && xhttp.status == 200) {
-     document.getElementById("search-result").innerHTML = xhttp.responseText;
+      title = '';
+      description = '';
+      var data = JSON.parse(xhttp.responseText, function(key, value) {
+          if (key == "title") {
+            valueAddUnderscore = value.replace(' ', '_');
+            title += "<div class='col-md-8 col-md-offset-2 list-group'><button type='button' class='list-group-item' onclick='window.open(\"https://en.wikipedia.org/wiki/"+valueAddUnderscore+"\", \"_blank\")' /><h2 class='col-md-offset-1'>"+value+"</h2>";
+          }
+          if (key == "extract") {
+            title += "<div class='col-md-10 col-md-offset-1'><div>"+value+"</div></div></button></div>";
+          }
+      }); 
+      document.getElementById('search-result').innerHTML = title;
+      document.getElementById('search-message').innerHTML = 'Displaying top 20 results';
     }
   };
-  xhttp.open("GET", url, true);
-  xhttp.send();*/
-
-  $.ajax({
-      type:"GET",
-      dataType:"jsonp",
-      url:searchLink,
-      success: function(result){
-          console.log(result);
-      }
-  });
+  xhttp.open("GET", searchLink, true);
+  xhttp.send();
 }
 
 function showSearchBar() {
   document.getElementById('icon').style.display = 'none';
   document.getElementById('search-bar').style.display = 'inline';
+  document.getElementById('search-btn').style.display = 'inline';
+}
+
+// Trying to get the url for an image to display with the link
+function getImageLink(title) {
+   var imageSearchLink = 'https://en.wikipedia.org/w/api.php?action=query&format=json&titles='+title+'&prop=pageimages&pithumbsize=100&origin=*';
+   var xhttp = new XMLHttpRequest();
+   xhttp.onreadystatechange = function() {
+     if (xhttp.readyState == 4 && xhttp.status == 200) {
+       var imgLink = '';
+       var data = JSON.parse(xhttp.responseText, function(key, value) {
+         imgArray = {};
+         imgArray[key] = value;
+         for (var i in imgArray) {
+           if (i == "url") {
+             imgLink = imgArray[i];
+           }
+         }
+         return imgLink;
+       });
+     }
+   };
+  xhttp.open("GET", imageSearchLink, true);
+  xhttp.send();
 }
